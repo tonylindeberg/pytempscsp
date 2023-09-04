1c816d6c25c07c48827063f246928cb8590ca5f4
benman1 <auffarth@gmail.com>
2023-09-04 12:41:55 +0100

    test for limitkern_sospars_2layers; found the problem!

--- test_tempscsp.py
+++ test_tempscsp.py
@@ -55,6 +55,88 @@ def test_mufromstddev():
     )
 
 
+def test_limitkern_composedsospars_alllayers():
+    "Test limitkern_composedsospars_alllayers."
+    muvec = np.array([
+        6.103143141433787e-05,
+        0.00018307195340983018,
+        0.0007318862175645924,
+        0.0029211543572212895,
+        0.011584548242028148,
+        0.04486236794258425,
+        0.16143782776614768,
+        0.5
+    ])
+    sospars_expected = np.array([[
+        9.99755945e-01,
+        0.00000000e+00,
+        0.00000000e+00,
+        1.00000000e+00,
+        -2.44066151e-04,
+        1.11704165e-08
+    ],[
+        9.96358133e-01,
+        0.00000000e+00,
+        0.00000000e+00,
+        1.00000000e+00,
+        -3.64399702e-03,
+        2.13016647e-06
+    ],[
+        9.46103666e-01,
+        0.00000000e+00,
+        0.00000000e+00,
+        1.00000000e+00,
+        -5.43880339e-02,
+        4.91699788e-04
+    ],[
+        5.74001165e-01,
+        0.00000000e+00,
+        0.00000000e+00,
+        1.00000000e+00,
+        -4.72331585e-01,
+        4.63327506e-02
+    ]])
+    assert np.allclose(
+        sospars_expected,
+        tempscsp.limitkern_composedsospars_alllayers(muvec)
+    )
+
+def test_limitkern_composedsospars_alllayers_list():
+    """Test tlimitkern_composedsospars_alllayers_list()."""
+    muvec = np.array(
+        [6.103143141433787e-05,
+         0.00018307195340983018,
+         0.0007318862175645924,
+         0.0029211543572212895,
+         0.011584548242028148,
+         0.04486236794258425,
+         0.16143782776614768,
+         0.5
+    ])
+    listformat_expected = [0.9997559450194062, 0.0, 0.0, 1.0, -0.00024406615101033618, 1.1170416507132956e-08, 0.9963581331461244,
+                 0.0, 0.0, 1.0, -0.003643997020350049, 2.1301664746710137e-06, 0.9461036658824216, 0.0, 0.0, 1.0,
+                 -0.054388033905137426, 0.0004916997875589257, 0.5740011653907472, 0.0, 0.0, 1.0, -0.4723315852472125,
+                 0.04633275063795974]
+    assert np.allclose(
+        listformat_expected,
+        tempscsp.limitkern_composedsospars_alllayers_list(muvec)
+    )
+
+
+def test_limitkern_sospars_2layers():
+    """test limitkern_sospars_2layers()."""
+    res_expected = np.array([
+        0.5740011653907472,
+        0.0,
+        0.0,
+        1.0,
+        -0.4723315852472125,
+        0.04633275063795974
+    ])
+    assert np.allclose(
+        res_expected,
+        tempscsp.limitkern_sospars_2layers(mu1=0.16143782776614768, mu2=0.5)
+    )
 
 if __name__ == "__main__":
-    test_sosfilt()
+    test_limitkern_sospars_2layers()
